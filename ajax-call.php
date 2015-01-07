ec17b31a505b233801667c1e1ffb0730dd264e2f
Fred J <fredjuvaux@users.noreply.github.com>
2015-01-07 15:48:04 +0100

    Update ajax-call.php

--- ajax-call.php
+++ ajax-call.php
@@ -3,8 +3,11 @@
 include_once('../../config/config.inc.php');
 include_once('../../init.php');
 
-public function send_email() {
+public function send_email_to_mailchimp() {
   return 'ok';
+}
+if ($_GET['ajax']){
+  echo function send_email_to_mailchimp();
 }
 
 ?>
